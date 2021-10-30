b83aeef170d208d2aca3ae760c1d7b4304c18999
fillipe.felix <felipesoares_1993@hotmail.com>
2021-10-30 11:27:41 -0300

    Primeiro principio e o mais importante do SOLID - Single Responsability Principe

---
+++ SOLID/1 - SRP/SRP.Solucao/ClienteService.cs
@@ -0,0 +1,20 @@
+﻿namespace SOLID._1___SRP.SRP.Solucao
+{
+    public class ClienteService
+    {
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.Validar())
+            {
+                return "Dados inválidos";
+            }
+
+            var repo = new ClienteRepository();
+            repo.AdicionarCliente(cliente);
+
+            EmailService.Enviar("empresa@teste.com", cliente.Email.Endereco, "Bem Vindo", "Parabéns! Você está cadastrado.");
+
+            return "Cliente cadastrado com sucesso";
+        }
+    }
+}
